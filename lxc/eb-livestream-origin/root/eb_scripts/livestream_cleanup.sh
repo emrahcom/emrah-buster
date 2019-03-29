3884d47c0acadbd59b5fcd1e4a56df2b012b94bc
emrah <emrah.com@gmail.com>
2019-03-29 17:26:03 +0300

    en-livestream: dash fragment extention for cleanup

--- lxc/eb-livestream-origin/root/eb_scripts/livestream_cleanup.sh
+++ lxc/eb-livestream-origin/root/eb_scripts/livestream_cleanup.sh
@@ -2,5 +2,5 @@
 
 find /usr/local/eb/livestream/hls/ -type f -name "*.ts" -cmin +10 -delete
 find /usr/local/eb/livestream/hls/ -type f -name "*.m3u8" -cmin +10 -delete
-find /usr/local/eb/livestream/dash/ -type f -name "*.mp4" -cmin +10 -delete
+find /usr/local/eb/livestream/dash/ -type f -name "*.m4?" -cmin +10 -delete
 find /usr/local/eb/livestream/dash/ -type f -name "*.mpd" -cmin +10 -delete
